12e4e7bb049015a039481654a5bac76c7533cf3f
helgaathifah05@gmail.com <helgaathifah05@gmail.com>
2026-02-18 15:25:38 +0700

    merapikan komentar

--- lib/features/logbook/counter_controller.dart
+++ lib/features/logbook/counter_controller.dart
@@ -7,28 +7,27 @@ class CounterController {
   int get value => _value;
   List<String> get history => _history;
 
-  // 1. Fungsi Membaca Data (Sekarang menerima parameter username)
+  // 1. Fungsi Membaca Data
   Future<void> loadData(String username) async {
     final prefs = await SharedPreferences.getInstance();
-    
-    // Kunci unik: misal 'admin_last_counter'
-    _value = prefs.getInt('${username}_last_counter') ?? 0; 
+
+    _value = prefs.getInt('${username}_last_counter') ?? 0;
     _history = prefs.getStringList('${username}_counter_history') ?? [];
   }
 
-  // 2. Fungsi Menambah & Menyimpan Data 
+  // 2. Fungsi Menambah & Menyimpan Data
   Future<void> increment(String username) async {
     _value++;
-    
+
     DateTime now = DateTime.now();
     String timeStr = "${now.hour}:${now.minute.toString().padLeft(2, '0')}";
-    
+
     String logEntry = "User $username menambah ke $_value pada jam $timeStr";
-    
+
     _history.insert(0, logEntry);
 
     final prefs = await SharedPreferences.getInstance();
-    
+
     await prefs.setInt('${username}_last_counter', _value);
     await prefs.setStringList('${username}_counter_history', _history);
   }
