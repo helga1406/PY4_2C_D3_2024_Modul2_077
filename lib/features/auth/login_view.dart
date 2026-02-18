7f060401d7136607be7f8eba9a3daa50ffae7bd3
helgaathifah05@gmail.com <helgaathifah05@gmail.com>
2026-02-18 15:21:42 +0700

    Update tampilan Login

--- lib/features/auth/login_view.dart
+++ lib/features/auth/login_view.dart
@@ -53,14 +53,27 @@ class _LoginViewState extends State<LoginView> {
     }
   }
 
-  @override
+@override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Login Gatekeeper")),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          "Login LogBook",
+          style: TextStyle(
+            color: Color.fromARGB(255, 158, 101, 140),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            const SizedBox(height: 20), 
             TextField(
               controller: _userController,
               decoration: const InputDecoration(
@@ -75,8 +88,6 @@ class _LoginViewState extends State<LoginView> {
               decoration: InputDecoration(
                 labelText: "Password",
                 border: const OutlineInputBorder(),
-
-                // 3. Tambahkan IconButton untuk toggle visibilitas password
                 suffixIcon: IconButton(
                   icon: Icon(_isObscure ? Icons.visibility_off : Icons.visibility),
                   onPressed: () {
@@ -87,11 +98,21 @@ class _LoginViewState extends State<LoginView> {
                 ),
               ),
             ),
-            const SizedBox(height: 20),
-            // 4. Tombol otomatis mati jika isLocked true 
+            const SizedBox(height: 30),
             ElevatedButton(
               onPressed: _controller.isLocked ? null : _handleLogin,
-              child: Text(_controller.isLocked ? "Terkunci (10 detik)" : "Masuk"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 158, 101, 140),
+                foregroundColor: Colors.white,
+                minimumSize: const Size(150, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              child: Text(
+                _controller.isLocked ? "Terkunci (10 detik)" : "Masuk",
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
             ),
           ],
         ),
@@ -99,4 +120,3 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 }
-
